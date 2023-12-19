8e5ad60eed0585fd5fcd4745763c5b59f45a0d06
juebrauer <juebrauer@gmail.com>
2023-12-19 20:59:18 +0100

    Example of a base class logger and derived classes LoggerFile and LoggerHTML

--- cpp/logger.cpp
+++ cpp/logger.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <iomanip>
+#include <chrono>
+#include <sstream>
+#include <vector>
 
 using namespace std;
 
+
+std::string getCurrentDateTime() {
+    // Aktuelle Zeit im system_clock (meistens Unix Zeit)
+    auto now = std::chrono::system_clock::now();
+
+    // Konvertieren in time_t für die Umwandlung in lokale Zeit
+    auto now_c = std::chrono::system_clock::to_time_t(now);
+
+    // Erstellen eines stringstream Objekts
+    std::stringstream ss;
+
+    // Verwenden von put_time, um Datum und Uhrzeit im gewünschten Format zu formatieren
+    ss << std::put_time(std::localtime(&now_c), "%Y-%m-%d %H:%M:%S");
+
+    // String zurückgeben
+    return ss.str();
+}
+
+
+
 class Logger
 {
-    private:
+    protected:
         string name;
+        int msg_counter;
 
 
     public:
@@ -14,38 +40,148 @@ class Logger
         // Konstruktor der Klasse
         Logger(string n)
         {
-            name = n;
-            cout << "Eine neue Instanz der Klasse Logger wurde erzeugt:" << name << "\n";            
+            name = n;            
+            msg_counter = 0;
+
+            cout << "Logger constructor called." << endl;
         }
 
         // Eine weitere Methode
-        void logmsg(string txt)
-        {
-            cout << name << " - Neue Lognachricht: " << txt << endl;
-        }
-
+        virtual void logmsg(string txt) = 0;
+       
         // Destruktor
-        ~Logger()
-        {
-            cout << "Ciao sagt " << name << "!\n";
+        virtual ~Logger() {
+
         }
 
 };
+
+
+class LoggerFile : public Logger
+{
+    private:
+    
+        ofstream outfile;
+        string   logfname;
+
+    public:
+
+        LoggerFile(string n, string logfilename) : Logger(n)
+        {
+            cout << "LoggerFile constructor called." << endl;
+            outfile.open( logfilename );
+            logfname = logfilename;
+        }
+
+        void logmsg(string txt)
+        {
+            string currentDateTime = getCurrentDateTime();
+            outfile << name << " | " << currentDateTime << " - " << txt << endl;
+            msg_counter++;
+        }
+
+        ~LoggerFile()
+        {
+            cout << "Schließe die Logdatei " << logfname << endl;
+            outfile.close();
+        }
+
+
+};
+
+class LoggerHTML : public Logger
+{
+    private:
+    
+        ofstream outfile;
+        string   logfname;
+
+    public:
+
+        LoggerHTML(string n, string logfilename, string websitename) : Logger(n)
+        {
+            cout << "LoggerHTML constructor called." << endl;
+            outfile.open( logfilename );
+            logfname = logfilename;
+
+            outfile << "<!DOCTYPE html>\n";
+            outfile << "<html>\n";
+            outfile << "   <head>\n";
+            outfile << "     <title>\n";
+            outfile << "         " << websitename << "\n";
+            outfile << "     </title>\n";
+            outfile << "   </head>\n";
+            outfile << "   <body>\n";
+            
+        }
+
+        void logmsg(string txt)
+        {
+            string currentDateTime = getCurrentDateTime();
+            outfile << "\t\t\t<p>" << 
+                       name << 
+                       " | " <<
+                       currentDateTime <<
+                       " - " << 
+                       txt <<
+                       "</p>" << endl;
+            msg_counter++;
+        }
+
+        ~LoggerHTML()
+        {            
+            cout << "Schließe die Logdatei / HTML-Datei " << logfname << endl;
+
+            outfile << "   </body>\n";
+            outfile << "</html>\n";
+            outfile.close();
+        }
+
+
+};
+
+
 
 int main()
 {
     cout << "Hello world!" << endl;
 
-    Logger* l1;
-    l1 = new Logger("L1");
-    l1->logmsg("Programm wird gestartet!");
+    vector<int> v;
+    v.push_back( 100 );
+    for (int i=0; i<10; i++)
+        v.push_back( 105 );
+    v.push_back( 95 );
+    
+    for (int i=0; i<v.size(); i++)
+        cout << v[i] << " ";  
 
+    /*
+    Logger* p = new Logger("L1");
+    p->logmsg("Hallo! Ich bin die Ausgabe aus der Basisklasse.");
+    */
+
+    /*
+
+    //LoggerFile* ptr_l1 = new LoggerFile("L1", "logfile.txt");
+    Logger* ptr_l1 = new LoggerHTML("L1",
+                                    "logfile.html",
+                                    "Dies ist eine Beispiel Logdatei in HTML!!!");
+    ptr_l1->logmsg("Programm wird gestartet!");
+
+    for (int i=1; i<=5; i++)
+    {
+        ptr_l1->logmsg( to_string(i) + " --> " + to_string(i*i) );
+    }
+
+    delete ptr_l1;
+    */
+
+    //l1->name = "Ätschibätch! Ich gebe dir einen anderen Name";
+
+    /*
     for (int i=0; i<5; i++)
         l1->logmsg( to_string(i) );
 
     l1->logmsg("Programm wird beendet!");
-
-    Logger l2 = Logger("L2");
-
-    Logger l3 = Logger("L3");
+    */
 }
